48a65ef875b24b0fbd847e6ae817cbc0b345b81c
rohit150800 <91822753+rohit150800@users.noreply.github.com>
2023-09-11 20:11:17 +0530

    Add files via upload

---
+++ mycaptain python1.py
@@ -0,0 +1,4 @@
+a=float(input("radius of the circle : "))
+area=(3.14*a*a)
+print("Area of the circle is ")
+print(area)
---
+++ mycaptain python2.py
@@ -0,0 +1,3 @@
+a=input("Input the filename : ")
+b=a.split(".")
+print("The extension of the file is : ",(b[-1]))
