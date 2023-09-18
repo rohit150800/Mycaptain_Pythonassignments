da23c5403bc5448862203f5a2bb9150fe55e2581
rohit150800 <91822753+rohit150800@users.noreply.github.com>
2023-09-18 22:49:47 +0530

    Add files via upload

---
+++ positive numbers.py
@@ -0,0 +1,10 @@
+print("L1 + numbers")
+list1 =[12, -7, 5, 64, -14]
+list2 =[12, 14, -95, 3]
+for num in list1:   
+	if num >= 0:
+		print(num)
+print("L2 + numbers")
+for num in list2:
+    if num >=0:
+        print(num)
